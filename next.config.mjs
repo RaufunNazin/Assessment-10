86eb7edbda1ba2d90d0451fc1163bec86257c353
RaufunNazin <raufun.nazin13@gmail.com>
2025-07-28 09:47:14 +0600

    add packages

---
+++ next.config.mjs
@@ -0,0 +1,30 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    domains: ['api.10minuteschool.com', 'img.youtube.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+    unoptimized: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/',
+        destination: '/en',
+      },
+    ];
+  },
+};
+
+export default nextConfig;
